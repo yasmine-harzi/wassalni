5bc154c76a0f14ed8494b87affe29ddc6343fd47
moatezdaknou <moatezdaknou456@gmail.com>
2026-04-05 21:29:47 +0100

    Update basededonnées_projweb.sql

--- basededonnées_projweb.sql
+++ basededonnées_projweb.sql
@@ -1,20 +1,42 @@
 create table users (
-    id_user int not null primary key,
+    id_user int primary key,
     nom varchar(50) not null,
     prenom varchar(50) not null,
     email varchar(100) unique not null,
     password varchar(255) not null,
     telephone varchar(20),
-    adresse varchar(20),
+    adresse varchar(50),
     role varchar(10) not null,
     date_creation date,
     check (role in ('client','vendeur','chauffeur','admin'))
 );
+create table admin (
+    id_admin int primary key,
+    niveau_acces varchar(20),
+    foreign key (id_admin) references users(id_user)
+);
+create table coursier (
+    id_coursier int primary key,
+    vehicule varchar(50),
+    disponibilite varchar(20),
+    foreign key (id_coursier) references users(id_user)
+);
+create table vendeur (
+    id_vendeur int primary key,
+    nom_entreprise varchar(100),
+    adresse_entreprise varchar(100),
+    foreign key (id_vendeur) references users(id_user)
+);
+create table client (
+    id_client int primary key,
+    type_client varchar(20),
+    foreign key (id_client) references users(id_user)
+);
 create table colis (
-    id_colis int not null primary key,
+    id_colis int primary key,
     description varchar(100) not null,
     poids float(6,2) not null,
-    statut varchar(20) default 'en attente',
+    statut varchar(20),
     date_creation date,
     date_livraison date,
     id_client int not null,
@@ -26,7 +48,7 @@ create table colis (
     foreign key (id_chauffeur) references users(id_user)
 );
 create table suivi_colis (
-    id_suivi int not null primary key,
+    id_suivi int primary key,
     id_colis int not null,
     latitude float(10,8) not null,
     longitude float(11,8) not null,
@@ -34,10 +56,10 @@ create table suivi_colis (
     foreign key (id_colis) references colis(id_colis)
 );
 create table notifications (
-    id_notification int not null primary key,
+    id_notification int primary key,
     message varchar(100) not null,
     date_envoi date,
-    statut varchar(10) ,
+    statut varchar(10),
     id_user int not null,
     id_colis int,
     check (statut in ('envoyee','lue')),
