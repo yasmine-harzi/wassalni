e701db4437d0718b312c3f773753217c0bf63f2d
yosraboughattas <yosraboughattas8@gmail.com>
2026-05-01 12:50:18 +0200

    complete "controle de saisie"
    j'ai completé l'inscription pour les 3 acteurs

--- wassali-frontend/src/app/pages/register-livreur/register-livreur.ts
+++ wassali-frontend/src/app/pages/register-livreur/register-livreur.ts
@@ -13,19 +13,34 @@ import { CommonModule } from '@angular/common';
 export class RegisterLivreur {
 
   coursierForm = new FormGroup({
-  // Champs pour la table 'users'
-  nom: new FormControl(''),// Validators.required),
-  prenom: new FormControl(''),
-  email: new FormControl(''),
-  password: new FormControl(''),
-  telephone: new FormControl(''),
-  adresse: new FormControl(''),// Domicile
+    nom: new FormControl('', Validators.required),
+    prenom: new FormControl('', Validators.required),
+    
+    // Email : Obligatoire + finit par @gmail.com
+    email: new FormControl('', [
+      Validators.required, 
+      Validators.pattern('^[a-z0-9._%+-]+@gmail\\.com$')
+    ]),
+
+    // Password : 8 car. + 1 Maj + 1 Chiffre + 1 Caractère spécial
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern('^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')
+    ]),
+
+    // Téléphone : 8 chiffres commençant par 2, 5 ou 9
+    telephone: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[259][0-9]{7}$')
+    ]),
+    adresse: new FormControl('', Validators.required),
   
   // Champs pour la table 'coursier' (liés par id_user au backend)
  
-  vehicule: new FormControl(''),
-  permis: new FormControl(''),
-  zone_livraison: new FormControl(''), // Travail
+  vehicule: new FormControl('', Validators.required),
+  permis: new FormControl('', Validators.required),
+  zone_livraison: new FormControl('', Validators.required), // Travail
   disponibilite: new FormControl(true),
   latitude_actuelle: new FormControl(0),
   longitude_actuelle: new FormControl(0)
--- wassali-frontend/src/app/pages/register-vendeur/register-vendeur.ts
+++ wassali-frontend/src/app/pages/register-vendeur/register-vendeur.ts
@@ -1,25 +1,41 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth'; 
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-register-vendeur',
   standalone: true,
-  imports: [ReactiveFormsModule,ReactiveFormsModule],
+  imports: [ReactiveFormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './register-vendeur.html',
   styleUrl: './register-vendeur.css'
 })
 export class RegisterVendeur {
 
   vendeurForm = new FormGroup({
-   nom: new FormControl(''),
-    prenom: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    telephone: new FormControl(''),
-    adresse: new FormControl(''),
-    nom_entreprise: new FormControl(''), // Correspond à ta DB
-    adresse_entreprise: new FormControl('') // Correspond à ta DB
+   nom: new FormControl('', Validators.required),
+    prenom: new FormControl('', Validators.required),
+    
+    // Email : Obligatoire + finit par @gmail.com
+    email: new FormControl('', [
+      Validators.required, 
+      Validators.pattern('^[a-z0-9._%+-]+@gmail\\.com$')
+    ]),
+
+    // Password : 8 car. + 1 Maj + 1 Chiffre + 1 Caractère spécial
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern('^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')
+    ]),
+
+    // Téléphone : 8 chiffres commençant par 2, 5 ou 9
+    telephone: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[259][0-9]{7}$')
+    ]),
+    adresse: new FormControl('', Validators.required),
+    nom_entreprise: new FormControl('', Validators.required), // Correspond à ta DB
+    adresse_entreprise: new FormControl('', Validators.required) // Correspond à ta DB
   });
 
   constructor(private monOutilAuth: AuthService) {}
